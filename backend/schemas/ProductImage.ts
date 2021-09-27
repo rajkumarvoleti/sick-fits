3940319d0ac4dee361986b9beb7ab7f9dc213683
rajkumarvoleti <voletirajkumar@gmail.com>
2021-09-27 21:09:22 +0530

    bug

--- backend/schemas/ProductImage.ts
+++ backend/schemas/ProductImage.ts
@@ -17,7 +17,7 @@ export const ProductImage = list({
     read: true,
     update: permissions.canManageProducts,
     delete: permissions.canManageProducts,
-  }
+  },
   fields: {
     image: cloudinaryImage({
       cloudinary,
